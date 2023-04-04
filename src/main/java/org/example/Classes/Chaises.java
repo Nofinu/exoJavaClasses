2831d856f69afbf974aae9d11e88804f4b6597cb
Leo Lhomme <leolhommeo@gmail.com>
2023-04-04 09:02:23 +0200

    creation repo

---
+++ src/main/java/org/example/Classes/Chaises.java
@@ -0,0 +1,37 @@
+package org.example.Classes;
+
+public class Chaises {
+    int nbPieds;
+    String couleur;
+    String materiaux;
+
+    public Chaises(int nbPieds, String couleur, String materiaux) {
+        this.nbPieds = nbPieds;
+        this.couleur = couleur;
+        this.materiaux = materiaux;
+    }
+
+    public Chaises() {
+    }
+
+    public int getNbPieds() {
+        return nbPieds;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public String getMateriaux() {
+        return materiaux;
+    }
+
+    @Override
+    public String toString() {
+        return "Chaises{" +
+                "nbPieds=" + nbPieds +
+                ", couleur='" + couleur + '\'' +
+                ", materiaux='" + materiaux + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Classes/MainChaises.java
@@ -0,0 +1,24 @@
+package org.example.Classes;
+
+import org.example.Classes.Chaises;
+
+public class MainChaises {
+
+    public static void main() {
+        Chaises chaise1 = new Chaises(4,"bleu","bois");
+        affichage(chaise1);
+
+        Chaises chaise2 = new Chaises(1,"blanche","metal");
+        affichage(chaise2);
+
+        Chaises chaise3 = new Chaises(3,"transparent","pléxiglass");
+        affichage(chaise3);
+
+    }
+
+    public static void affichage(Chaises objet) {
+
+        System.out.println("--------------- Affichage d'un nouvelle objet ---------------");
+        System.out.println("je suis une Chaise, avec "+objet.getMateriaux()+" pieds en "+objet.getMateriaux()+" est de couleur "+objet.getCouleur());
+    }
+}
